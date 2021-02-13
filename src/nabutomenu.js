10dc631d7817b812af957ab4dc3bfdbfb3f980c8
flavio357 <78444406+flavio357@users.noreply.github.com>
2021-02-13 18:21:18 -0300

    Add files via upload

---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const nabutomenu = (prefix, pushname) => {
+    return `◪ *Comandos da Charlotte*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabutomenu
